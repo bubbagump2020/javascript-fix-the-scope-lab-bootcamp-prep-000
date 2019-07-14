a8c29e429ea60ff53a244338da56fc5a329163c7
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 00:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function add2(n) {
 }
 
 function funkyFunction() {
- return "FUNKY!"
+ function funky(){
+   return "FUNKY!"
+ }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
